5a9c29f08a99214b0f7cfe3943edf8723a6e2008
WaiYanHtun <waiyanhtun282@gmail.com>
2026-05-01 06:08:56 +0400

    529fileterdebouncinffinisishing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
+import { Link, useSearchParams } from 'react-router';
 const users = [
   {
     id: 1,
@@ -19,15 +20,36 @@ const users = [
   }
 ];
 // let timer:number;
-function App() {
-  const [inputValue,setInputValue]=useState('');
 
-  const filterUsers = users.filter((user) => user.name.toLowerCase().includes(inputValue.toLowerCase()));
+function App() {
+  const [searchParams ,setSearchParams] = useSearchParams();
+ const searchQuery = searchParams.get('search') || '';
+  const [inputValue,setInputValue]=useState(searchQuery);
+  // const [searchQuery,setSearchQuery]=useState('');
+  useEffect(
+    ()=>{
+  const timer = setTimeout(
+    ()=>{
+    if(inputValue){
+          searchParams.set('search',inputValue);
+    }else {
+      searchParams.delete('search');  
+    }
+    setSearchParams(searchParams);
+  },500);
+
+  return () =>  clearTimeout(timer);
+
+   
+},[inputValue ,searchParams,setSearchParams]);
+
+  const filterUsers = users.filter((user) => user.name.toLowerCase().includes(searchQuery.toLowerCase()));
 
   
   
   return (
     <div className='card'>
+      <Link to="/help">Link to other page</Link>
 
     <h3>Search Filtering and Debouncing</h3>
     <input type="text" placeholder="Search users..."  value={inputValue} onChange={(e) => setInputValue(e.target.value)}   />
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { LanguageProvider } from './providers/LaungageContes.tsx'
+// import { LanguageProvider } from './providers/LaungageContes.tsx'
+import { BrowserRouter,Routes,Route } from 'react-router'
+import Help from './Help.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LanguageProvider>
-    <App />
-    </LanguageProvider>
+    <BrowserRouter>
+    {/* <LanguageProvider> */}
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/help" element={<Help />}  />
+    
+    </Routes>
+    {/* </LanguageProvider> */}
+    </BrowserRouter>
+
   </StrictMode>,
 )
